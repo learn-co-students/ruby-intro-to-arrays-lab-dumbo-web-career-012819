2b993346cac244811fe7ce980e06ea2c1706c4fb
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2019-01-04 00:35:47 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+# working with arrays
+
+def instantiate_new_array
+my_array = Array.new
+return my_array
+end
+
+def array_with_two_elements
+my_array = ["cat","dog"]
+return my_array
+end
+
+def first_element(taylor_swift)
+  return taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  return taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  return taylor_swift[-1]
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia.last
+end
+
+def length_of_array(programming_languages)
+  programming_languages.size
+end
